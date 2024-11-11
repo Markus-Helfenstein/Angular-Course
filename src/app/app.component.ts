e4c271c3c49db234d08e72f408510e54bd65715e
Markus Helfenstein <markus.helfenstein@gmail.com>
2024-11-11 11:51:38 +0100

    Personal implementation of task in lecture 37

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUser = signal<{ id: string, name: string, avatar: string } | undefined>(undefined);
+
+  onSelectUser(selectedUserId: string) {
+    console.log(`Selected user with id ${selectedUserId}`);
+    this.selectedUser.set(DUMMY_USERS.find(u => u.id === selectedUserId));
+  }
 }
